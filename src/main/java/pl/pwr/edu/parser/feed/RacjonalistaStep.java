ed21defe81714caa16da9ab4dea86230027539cf
Mariusz <mariusz.gilewicz@gmail.com>
2017-03-31 14:17:32 +0200

    Date, multiple article pages, need to fix thread sleeping to stop getting 403s

--- src/main/java/pl/pwr/edu/parser/feed/RacjonalistaStep.java
+++ src/main/java/pl/pwr/edu/parser/feed/RacjonalistaStep.java
@@ -2,6 +2,8 @@ package pl.pwr.edu.parser.feed;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.TextNode;
 import pl.pwr.edu.parser.model.Article;
 import pl.pwr.edu.parser.model.RacjonalistaArticle;
 
@@ -20,46 +22,79 @@ public class RacjonalistaStep implements Step {
     public List<Article> parse() {
         List<Article> articles = new ArrayList<>();
 
-        try {
-            List<String> links = getArticlesLinks();
-            links.forEach(link -> articles.add(parseLink(link)));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        List<String> links = getArticlesLinks();
+        links.forEach(link -> {
+            try {
+                Article article = parseLink(link);
+                articles.add(article);
+                System.out.println(article);
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        });
 
         return articles;
     }
 
-    private List<String> getArticlesLinks() throws IOException {
-        Document doc = Jsoup.connect(articleListUrl).get();
+    private List<String> getArticlesLinks() {
+        List<String> links = new ArrayList<>();
 
-        return doc.select("#oTxt").first()
-                .select(".linkart")
-                .stream()
-                .map(link -> link.attr("href"))
-                .collect(Collectors.toList());
+        try {
+            Document doc = Jsoup.connect(articleListUrl).get();
+            links.addAll(doc.select("#oTxt").first()
+                    .select(".linkart")
+                    .stream()
+                    .map(link -> link.attr("href"))
+                    .collect(Collectors.toList()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return links;
     }
 
     private Article parseLink(String articleUrl) {
         RacjonalistaArticle article = new RacjonalistaArticle();
         try {
             Document doc = Jsoup.connect(baseUrl + articleUrl).get();
+
             article.setTitle(doc.select("meta[property=og:title]").attr("content"));
-
-            HashMap<String, String> metaData = new HashMap<>();
-            metaData.put("author", doc.select("a[rel=author]").text());
-            metaData.put("category", doc.select(".linkdzial").last().getElementsByTag("b").text());
-            metaData.put("keywords", doc.select("meta[name=keywords]").attr("content"));
-            metaData.put("description", doc.select("meta[name=description]").attr("content"));
-
-
-            article.setMetadata(metaData);
-            article.setBody(doc.select("#oTxt").first().select("p").text().trim());
+            parseArticleMetaData(article, doc);
+            article.setBody(String.join("\n", parseArticleBody(doc, null)));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-
         return article;
+    }
+
+    private void parseArticleMetaData(RacjonalistaArticle article, Document doc) {
+        HashMap<String, String> metaData = new HashMap<>();
+        Element category = doc.select(".linkdzial").last();
+        metaData.put("author", doc.select("a[rel=author]").text().trim());
+        metaData.put("category", category.getElementsByTag("b").text().trim());
+        metaData.put("date", Jsoup.parse(category.nextSibling().nextSibling().toString()).text().trim());
+        metaData.put("keywords", doc.select("meta[name=keywords]").attr("content").trim());
+        metaData.put("description", doc.select("meta[name=description]").attr("content").trim());
+
+        article.setMetadata(metaData);
+    }
+
+    private List<String> parseArticleBody(Document doc, List<String> pages) throws IOException {
+        if (pages == null)
+            pages = new ArrayList<>();
+
+        pages.add(doc.select("#oTxt").first().select("p").text().trim());
+
+        Element nextPageButton = doc.select(".pg").last();
+        if (nextPageButton != null && nextPageButton.childNode(0) instanceof TextNode) {
+            String nextPageLink = nextPageButton.attr("href");
+            Document newPageDoc = Jsoup.connect(baseUrl + nextPageLink).get();
+            parseArticleBody(newPageDoc, pages);
+        }
+
+        return pages;
     }
 }
